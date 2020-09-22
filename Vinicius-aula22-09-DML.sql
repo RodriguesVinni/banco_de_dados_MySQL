6a40ca080c57c15ec9a80c6e2ace2d93428320a4
unknown <vinicius22d@gmail.com>
2020-09-22 12:07:12 -0300

    Aula curso generation Banco de Dados

--- Vinicius-aula22-09-DML.sql
+++ Vinicius-aula22-09-DML.sql
@@ -2,16 +2,16 @@ create database db_deletar;
 use db_deletar;
 
 #drop table consumidores;
-create table consumidores (
+create table if not exists consumidores (
 	id int PRIMARY KEY AUTO_INCREMENT,
     nome varchar(30)
     
     );
     
     insert into consumidores (nome) values
-    ("Marcelo"),
-    ("Julia");
+    ("Marcelo"), ("Julia");
     
 select * from consumidores;
 delete from consumidores where id = 1;
 select * from consumidores;
+#truncate consumidores;--> serve para limpar os somente os dados da tabela e mantendo sua estrutura 
